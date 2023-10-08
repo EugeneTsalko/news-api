72ef3a1e33d10c0842a250842f092939fb17d780
EugeneTsalko <tefworkmail@gmail.com>
2023-10-09 00:21:54 +0300

    style: add css in Article

--- app/components/Article/Article.tsx
+++ app/components/Article/Article.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { News } from '@/models/types';
+import Link from 'next/link';
 import styles from './Article.module.scss';
 
 type Props = {
@@ -7,22 +8,20 @@ type Props = {
 };
 
 function Article({ article }: Props) {
-  const { author, urlToImage, title, description, publishedAt } = article;
+  const { author, urlToImage, title, description, publishedAt, url } = article;
   return (
     <article className={styles.article}>
-      {urlToImage && <img src={urlToImage} alt={title} width={250} height={250} />}
-      <div>
-        <div>
-          <h2>{article.title}</h2>
-          <section>
-            <p>{description}</p>
-          </section>
-          <footer>
-            <p>{author && author} - </p>
-            <p>{publishedAt}</p>
-          </footer>
-        </div>
+      {urlToImage && <img src={urlToImage} alt={title} />}
+      <div className={styles.info}>
+        <h2>{article.title}</h2>
+        <p className={styles.articleDescr}>{description}</p>
+        <p className={styles.articleAuthor}>
+          {author && author} - <span>{new Date(publishedAt).toLocaleString()}</span>
+        </p>
       </div>
+      <Link href={url} target="_blank" className={styles.readMore}>
+        <span>READ MORE</span>
+      </Link>
     </article>
   );
 }
