d457b8b260fa5c4daf9c3c5895929f89f8728e84
EugeneTsalko <tefworkmail@gmail.com>
2023-10-15 20:45:18 +0300

    feat: add search on input change with debounce

--- src/app/components/SearchBox/SearchBox.tsx
+++ src/app/components/SearchBox/SearchBox.tsx
@@ -1,25 +1,32 @@
 'use client';
 
-import React, { FormEvent, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import Image from 'next/image';
+import useDebounce from '@/utils/useDebounce';
 import styles from './SearchBox.module.scss';
 
 function SearchBox() {
   const [input, setInput] = useState('');
   const router = useRouter();
+  const debouncedInput = useDebounce(input, 1000);
 
-  const handleSearch = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    router.push(`?q=${input}`);
-  };
+  const handleSearch = useCallback(() => {
+    if (debouncedInput) {
+      router.push(`?q=${debouncedInput}`);
+    }
+  }, [debouncedInput, router]);
+
+  useEffect(() => {
+    handleSearch();
+  }, [debouncedInput, handleSearch]);
 
   return (
-    <form className={styles.searchBox} onSubmit={handleSearch}>
+    <form className={styles.searchBox}>
       <input type="text" placeholder="Search keywords..." value={input} onChange={(e) => setInput(e.target.value)} />
-      <button type="submit" disabled={!input}>
+      <span className={!input ? styles.searchBox__disabled : ''}>
         <Image src="/Search.svg" width={24} height={24} alt="search" />
-      </button>
+      </span>
     </form>
   );
 }
